635f514f4e6ea69ac34abee65cf4bab86036d162
Hyeon <dolphin@kakao.com>
2025-05-07 20:24:26 +0900

    Element 변수 할당

--- 6기 박현수/script.js
+++ 6기 박현수/script.js
@@ -1,1 +1,31 @@
-console.log("hi");
+// 연산자
+const $plus = document.querySelector("[data-btn-plus]");
+const $minus = document.querySelector("[data-btn-minus]");
+const $multiply = document.querySelector("[data-btn-multiply]");
+const $divide = document.querySelector("[data-btn-divide]");
+const $eqaul = document.querySelector("[data-btn-eqaul]");
+
+// AC, DEL
+const $reset = document.querySelector("[data-btn-reset]");
+const $delete = document.querySelector("[data-btn-delete]");
+
+// 숫자
+const $numbers = document.querySelectorAll("[data-btn-number]");
+const $operations = document.querySelectorAll("[data-btn-operation]");
+
+const $previousPreview = document.querySelector("[data-previous-preview]");
+const $currentPreview = document.querySelector("[data-current-preview]");
+
+// 숫자 선택
+$numbers.forEach(($number) => {
+    $number.addEventListener("click", (e) => {
+        console.log(e.target.innerHTML);
+    });
+});
+
+// 연산자 선택택
+$operations.forEach(($operation) => {
+    $operation.addEventListener("click", (e) => {
+        console.log(e.target.innerHTML);
+    });
+});
